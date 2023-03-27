b36c47276d17dfe4ca484fe09ecba18ab7fdf96a
red2fred2 <red2fred2base@live.com>
2023-03-27 01:00:05 -0500

    Remove main timing

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,20 @@ extern crate test;
 mod huffman_tree;
 
 use huffman_tree::HuffmanTree;
-use std::{fs, /*time::Instant*/};
+use std::fs;
 
 const TEXT_FILE_PATH: &str = "2022_fall-eecs660-pa2-input.txt";
 
 fn main() {
 	let text = fs::read_to_string(TEXT_FILE_PATH)
 		.expect("Failed to read file");
-
-	// let timer = Instant::now();
-
 	let tree = HuffmanTree::new(&text)
 		.expect("Failed to build Huffman tree");
 	let bits = tree.encode(&text)
 		.expect("Failed to encode message");
 
 	let num_bits = bits.len();
-
-	// let time = timer.elapsed().as_micros();
 	println!("{num_bits} bits");
-	// println!("time: {time}us")
 }
 
 mod benchmarks {
